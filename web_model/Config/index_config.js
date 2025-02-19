50193f6c8e705fba138da9d9b33d7cc14bf2a5f3
guotaowei <guotaowei@tencent.com>
2025-02-19 16:50:29 +0800

    模型项目开发-主页

---
+++ web_model/Config/index_config.js
@@ -0,0 +1,51 @@
+// index_config.js
+export const moduleConfig = {
+  sections: [
+    {
+      title: "热门游戏",
+      items: [
+        {
+          buttonText: "可以点击",
+          imageUrl: "./images/game1.jpg",
+          description: "<p>国民级MOBA手游，<strong>腾讯最热门的游戏之一</strong></p>",
+          link: ""
+        },
+        {
+          buttonText: "和平精英",
+          imageUrl: "./images/game2.jpg",
+          description: "<p>超人气<em>生存射击手游</em>，快节奏的战术竞技体验</p>",
+          link: ""
+        },
+        {
+          buttonText: "王者荣耀",
+          imageUrl: "./images/game1.jpg",
+          description: "<p>国民级MOBA手游，<strong>腾讯最热门的游戏之一</strong></p>",
+          link: ""
+        },
+        {
+          buttonText: "和平精英",
+          imageUrl: "./images/game2.jpg",
+          description: "<p>超人气<em>生存射击手游</em>，快节奏的战术竞技体验</p>",
+          link: ""
+        },
+      ]
+    },
+    {
+      title: "推荐应用",
+      items: [
+        {
+          buttonText: "微信",
+          imageUrl: "./images/app1.jpg",
+          description: "<p>超过<strong>10亿</strong>用户的社交平台</p>",
+          link: ""
+        },
+        {
+          buttonText: "QQ音乐",
+          imageUrl: "./images/app2.jpg",
+          description: "<p>专业的<em>音乐流媒体服务</em></p>",
+          link: ""
+        },
+      ]
+    }
+  ]
+};
